89a06a48f73af15e93ac20315e0fe43a7df6c716
virgiawankusuma <virgiawan150@gmail.com>
2023-07-24 20:38:36 +0700

    ✨ feat: Create card component

---
+++ src/component/CardText.tsx
@@ -0,0 +1,12 @@
+export default function CardText() {
+  return <p 
+            style={{
+              width: '100%',
+              fontSize: '20px',
+              fontWeight: 'bold',
+              textAlign: 'left',
+            }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>;
+}
