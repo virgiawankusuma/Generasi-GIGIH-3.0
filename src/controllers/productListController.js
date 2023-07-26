210c21642c498f99b1b5c31482c621be6e007391
virgiawankusuma <virgiawan150@gmail.com>
2023-07-26 23:36:11 +0700

    ✨ feat: Add get product by VideoID

--- src/controllers/productListController.js
+++ src/controllers/productListController.js
@@ -1,12 +1,27 @@
 const productList = require('../models/Products');
+const videoThumbnailList = require('../models/VideoThumbnail');
 
 const getProducts = async (req, res) => {
   try {
-    const products = await productList.getProducts();
+    const products = await productList.find({});
     res.json(products);
   } catch (error) {
     res.status(500).json({ message: 'Error retrieving products', error });
   }
 }
 
-module.exports = { getProducts };
+const getProductByVideoID = async (req, res) => {
+  try {
+    const { VideoID } = req.params;
+    
+    const videoThumbnail = await videoThumbnailList.findOne({ VideoID });
+    const products = await productList.find({ ProductID: videoThumbnail.VideoID });
+
+    res.json(products);
+
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving product', error });
+  }
+}
+
+module.exports = { getProducts, getProductByVideoID };
--- src/routes/productListRoutes.js
+++ src/routes/productListRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productController = require('../controllers/productListController');
 
 router.get('/', productController.getProducts);
+router.get('/:VideoID', productController.getProductByVideoID);
 
 module.exports = router;
