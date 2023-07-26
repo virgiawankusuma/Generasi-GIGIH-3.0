4c620d9b4ea6efe2d481bb6d7bc863cced77c0a7
virgiawankusuma <virgiawan150@gmail.com>
2023-07-26 12:20:11 +0700

    ✨ feat: add picsum.photos domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      'images.unsplash.com',
+      'images.pexels.com',
+      'picsum.photos',
+    ],
+  },
+}
 
 module.exports = nextConfig
