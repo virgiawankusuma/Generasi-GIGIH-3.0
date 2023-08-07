d0388335d6c3b59164201f745dae12b3e1653143
virgiawankusuma <virgiawan150@gmail.com>
2023-08-07 20:06:22 +0700

    ✨ feat: Using env to set port

--- src/app.js
+++ src/app.js
@@ -28,6 +28,6 @@ app.use('/products', productsRoutes);
 app.use('/comments', commentRoutes);
 
 // Start the server
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running on port 3000');
 });
