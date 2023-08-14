1d38da335852c9c24b037ee07ebe425fe57d4e50
virgiawankusuma <virgiawan150@gmail.com>
2023-08-14 23:59:33 +0700

    ✨ feat: Change console.log

--- backend/src/app.js
+++ backend/src/app.js
@@ -29,5 +29,5 @@ app.use('/comments', commentRoutes);
 
 // Start the server
 app.listen(process.env.PORT, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port ' + process.env.PORT);
 });
