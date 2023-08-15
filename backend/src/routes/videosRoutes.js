c13282da6605f7325f22b07f79772acd18fa2ddb
virgiawankusuma <virgiawan150@gmail.com>
2023-08-15 07:32:38 +0700

    🐞 fix: Rename

--- backend/src/routes/videosRoutes.js
+++ backend/src/routes/videosRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const videosController = require('../controllers/videosController');
 
 router.get('/', videosController.getVideos);
-router.get('/:VideoID', videosController.getVideo);
+router.get('/:VideoID', videosController.getVideoById);
 
 module.exports = router;
